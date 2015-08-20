db8490b8d51e59fccb650f074d51c272200d80fe
Daniel Lima <daniel@docode.com.br>
2015-08-20 17:59:19 -0300

    Adicionado funcionalidade de busca por valores exatos e do tipo LIKE

--- src/dlimars/laravel-searchable/Searchable.php
+++ src/dlimars/laravel-searchable/Searchable.php
@@ -14,6 +14,29 @@ trait Searchable {
 	 */
 	public function scopeSearch(Builder $queryBuilder, $fields=[]){
 
+		if (isset($this->searchable)) {
+
+			foreach ($fields as $field => $value) {
+
+				if (array_key_exists($field, $this->searchable)) {
+
+					switch ($this->searchable[$field]) {
+						// compare equals values
+						case 'MATCH':
+							$queryBuilder->where($field, $value);
+							break;
+						
+						// compare like values
+						case 'LIKE':
+							array_map(function($value) use ($queryBuilder, $field){
+								$queryBuilder->where($field, "LIKE", "%".$value."%");
+							}, explode(" ", $value));
+							break;
+					}
+				}
+			}
+		}
+		return $queryBuilder;
 	}
 
 }
--- tests/TraitSearchableTest.php
+++ tests/TraitSearchableTest.php
@@ -4,22 +4,64 @@
 * 
 */
 class StubClass {
+	public $searchable;
 	use Dlimars\LaravelSearchable\Searchable;
 }
 
 class TraitSearchableTest extends PHPUnit_Framework_TestCase {
 
-	private $stubClass;
+	public $stubClass;
 
 	public function setUp(){
 		$this->stubClass = new StubClass;
 	}
 
-	public function testFail(){
-		
+	public function testCallLikeOperator(){
+		$this->stubClass->searchable = [ 'name' => 'LIKE'];
+		$search = ['name' => 'user'];
+
+		$query = $this->getBuilderMock();
+
+		$query->expects($this->once())
+			  ->method("where")
+			  ->with("name", "LIKE", "%user%");
+
+		$this->stubClass->scopeSearch($query, $search);
+	}
+
+	public function testCallLikeOperatorNTimes(){
+		$this->stubClass->searchable = [ 'name' => 'LIKE'];
+		$search = ['name' => 'user name'];
+
+		$query = $this->getBuilderMock();
+
+		$query->expects($this->at(0))
+			  ->method("where")
+			  ->with("name", "LIKE", "%user%");
+
+		$query->expects($this->at(1))
+			  ->method("where")
+			  ->with("name", "LIKE", "%name%");
+
+		$this->stubClass->scopeSearch($query, $search);
+	}
+
+	public function testCallMatchOperator(){
+		$this->stubClass->searchable = [ 'name' => 'MATCH'];
+		$search = ['name' => 'user'];
+
+		$query = $this->getBuilderMock();
+
+		$query->expects($this->once())
+			  ->method("where")
+			  ->with("name", "user");
+
+		$this->stubClass->scopeSearch($query, $search);
 	}
 
 	public function getBuilderMock(){
-		return $this->getMock("\Illuminate\Database\Eloquent\Builder");
+		return $this->getMock("\Illuminate\Database\Eloquent\Builder",[
+								'where'
+							]);
 	}
 }
